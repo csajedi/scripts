6346c88c3e858204d8561126b03cf5060d3c3e8a
Cameron Sajedi <cameronsajedi@gmail.com>
2020-09-01 20:21:54 -0400

    needed gcc

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 # Dependencies
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python-openssl git zsh
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git zsh gcc
 
 chsh -s $(which zsh)
 
