4b030a333d0d15a4eb277f1fc1b50c4f035bfcaa
Cameron Sajedi <cameronsajedi@gmail.com>
2020-09-01 20:08:01 -0400

    rocket fuel

--- installer.sh
+++ installer.sh
@@ -20,3 +20,18 @@ echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm"
 pyenv install 3.7.5
 pyenv global 3.7.5
 nvm install 12
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+sudo usermod --append --groups docker "$USER"
+sudo systemctl enable docker
+
+sleep 5
+
+# Docker Compose
+sudo wget --output-document=/usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.25.4/run.sh
+sudo chmod +x /usr/local/bin/docker-compose
+sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+printf '\nDocker Compose installed successfully\n\n'
