dd8cb21fdc418683540114ea0b53db0ccbfd6833
Cameron Sajedi <cameronsajedi@gmail.com>
2024-06-30 13:28:06 -0400

    add ohmyzsh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,8 @@ sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git gcc zlib1g zlib1g-dev \
 make zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 
 curl https://pyenv.run | bash
